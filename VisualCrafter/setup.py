b5f7f18b516f8c1c1d6173c53bf004cdd2b95a6f
digantsharma <sharmadigant1998@gmail.com>
2025-05-04 21:46:36 +0530

    Fix superuser creation

--- VisualCrafter/setup.py
+++ VisualCrafter/setup.py
@@ -1,22 +1,23 @@
 import os
 import django
 from django.core.management import call_command
-from django.contrib.auth.hashers import make_password
 from django.contrib.auth import get_user_model
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django.setup()
 
-def run_setup():
-    call_command('migrate')
-
+def create_superuser():
     User = get_user_model()
-    if not User.objects.filter(username='admin').exists:
+    if not User.objects.filter(username='admin').exists():
         User.objects.create_superuser(
             username='admin',
             email='admin@example.com',
-            password=make_password('Crafter@8')
+            password=os.getenv('ADMIN_PASSWORD', 'defaultpassword123')
         )
+        print("Superuser created successfully.")
+    else:
+        print("Superuser already exists.")
 
 if __name__ == '__main__':
-    run_setup()
+    call_command('migrate')
+    create_superuser()
