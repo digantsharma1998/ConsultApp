7ca5c60a84ac91f3fada64bbe8dbb9b71325cb38
digantsharma <sharmadigant1998@gmail.com>
2025-05-05 22:19:23 +0530

    Serializers for thumbnail preview

---
+++ VisualCrafter/resumes/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import ResumeTemplate
+from django.conf import settings
+
+class ResumeTemplateSerializer(serializers.ModelSerializer):
+    thumbnail = serializers.SerializerMethodField()
+
+    class Meta:
+        model = ResumeTemplate
+        fields = ['id', 'name', 'is_premium', 'thumbnail']
+
+    def get_thumbnail(self, obj):
+        if obj.thumbnail:
+            return self.context['request'].build_absolute_uri(obj.thumbnail.url)
+        return True
